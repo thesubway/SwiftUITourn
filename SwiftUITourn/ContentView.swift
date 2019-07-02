a9a298055b23c5f1d3ccd13e6f90d3318449aaa6
thesubway <thesubway@gmail.com>
2019-07-02 16:21:42 -0700

    Initial Commit

---
+++ SwiftUITourn/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUITourn
+//
+//  Created by Daniel Hoang on 7/2/19.
+//  Copyright © 2019 Daniel Hoang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
